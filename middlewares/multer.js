35bddca06a7cb4a90fa73bbb935eaeca8fc90816
Zaidan syahrani <zdnsyrhn22@gmail.com>
2022-01-01 17:30:58 +0700

    master

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 const storageMultiple = multer.diskStorage({
   destination: function (req, file, cb) {
-    var dir = 'public/images';
+    var dir = "public/images";
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
@@ -13,37 +13,35 @@ const storageMultiple = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
-  }
-})
+  },
+});
 
 const uploadMultiple = multer({
   storage: storageMultiple,
-  limits: { fileSize: 1000000 },
+  limits: { fileSize: 3000000 },
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
-  }
+  },
 }).array("image", 12);
-
 
 // Set storage engine
 const storage = multer.diskStorage({
   destination: "public/images",
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
-  }
+  },
 });
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1000000 },
+  limits: { fileSize: 3000000 },
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
-  }
+  },
 }).single("image");
 
 // // Check file Type
 function checkFileType(file, cb) {
-
   // Allowed ext
   const fileTypes = /jpeg|jpg|png|svg|gif/;
   // Check ext
